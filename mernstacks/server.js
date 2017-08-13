3dde4485eb50c345ee1ca3e98f513c78a60c1b27
plearnio <p.plearn.io@gmail.com>
2017-08-13 16:32:59 +0700

    config 1

--- mernstacks/server.js
+++ mernstacks/server.js
@@ -1,13 +1,14 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const book = require('./route/book')
 
 const app = express()
 
-app.use('/book', book)
-app.use(express.static(path.join(__dirname, '/public')))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use('/book', book)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
